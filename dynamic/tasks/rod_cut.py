13d4142155b120304513c394f7a38f81b3f6310e
Serhii Sobolev <sergiy.sobolev@gmail.com>
2019-06-14 21:35:59 +0300

    [DYNAMIC] Rod Cut Bottom Up with memoization

--- dynamic/tasks/rod_cut.py
+++ dynamic/tasks/rod_cut.py
@@ -4,15 +4,41 @@ class RodCut:
 
     def __init__(self, prices):
         self.prices = prices
+        self.memoized_result = {0:0}
         FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s()] %(message)s"
         log.basicConfig(format=FORMAT, level=log.DEBUG)
 
-    def find_optimum_cut(self, length=10):
+    def find_optimum_cut_top_down_recursive(self, length=10):
         log.debug(f'Looking for optimum cut for length =  {length}')
         if length == 0:
             return 0
-        q = -1000
+        q = 1
         for i in range(1,length+1):
-            q = max(q, self.prices[i] + self.find_optimum_cut(length-i))
+            q = max(q, self.prices[i] + self.find_optimum_cut_top_down_recursive(length - i))
         log.debug(f'Optimum cut for length {length} is {q}')
         return q
+
+    def find_optimum_cut_top_down_memoization(self, length=10):
+        pass
+
+    def find_optimum_cut_bottom_up(self, length=10):
+        self.fill_rod_cut_memoized_result(length)
+        return self.memoized_result[length]
+
+    def fill_rod_cut_memoized_result(self, length=10):
+        for i in range(0, length+1):
+            self.memoized_result[i] = self.calculate_memoized_rod_cut(i)
+
+    def calculate_memoized_rod_cut(self, length=10):
+        if length == 0:
+            return 0
+        q = 1
+        for i in range(1,length + 1):
+            q = max(q, self.prices[i] + self.memoized_result[length - i])
+        return q
+
+
+
+
+
+
--- dynamic/tests/test_rod_cut.py
+++ dynamic/tests/test_rod_cut.py
@@ -20,7 +20,7 @@ class TestRodCut(unittest.TestCase):
         ['10', 10, 30]
 
     ])
-    def test_rod_cut_optimum_1(self, name, length, price):
+    def test_rod_cut_optimum_down_recursive(self, name, length, price):
         prices = {
             1:1,
             2:5,
@@ -35,4 +35,34 @@ class TestRodCut(unittest.TestCase):
 
         }
         rc = RodCut(prices)
-        self.assertEqual(rc.find_optimum_cut(length), price)
+        self.assertEqual(rc.find_optimum_cut_top_down_recursive(length), price)
+
+    @parameterized.expand([
+        ['1', 1, 1],
+        ['2', 2, 5],
+        ['3', 3, 8],
+        ['4', 4, 10],
+        ['5', 5, 13],
+        ['6', 6, 17],
+        ['7', 7, 18],
+        ['8', 8, 22],
+        ['9', 9, 25],
+        ['10', 10, 30]
+
+    ])
+    def test_rod_cut_optimum_down_memoization(self, name, length, price):
+        prices = {
+            1: 1,
+            2: 5,
+            3: 8,
+            4: 9,
+            5: 10,
+            6: 17,
+            7: 17,
+            8: 20,
+            9: 24,
+            10: 30
+
+        }
+        rc = RodCut(prices)
+        self.assertEqual(rc.find_optimum_cut_bottom_up(length), price)
