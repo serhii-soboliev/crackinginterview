71c4cf20dc7b8d4c632aa5c31b54bc8feffaa1e2
Serhii Sobolev <sergiy.sobolev@gmail.com>
2019-06-15 17:30:16 +0300

    [DYNAMIC] Rod Cut problem with solution

--- dynamic/tasks/rod_cut.py
+++ dynamic/tasks/rod_cut.py
@@ -43,6 +43,26 @@ class RodCut:
             q = max(q, self.prices[i] + self.memoized_result[length - i])
         return q
 
+    def find_optimum_cut_bottom_up_with_first_piece(self, length=10):
+        r = {0:0}
+        s = {0:0}
+        for j in range(1, length+1):
+            q = -1
+            for i in range(1, j+1):
+                if q < self.prices[i] + r[j-i]:
+                    q = self.prices[i] + r[j-i]
+                    s[j] = i
+            r[j] = q
+        return r,s
+
+    def find_rod_cut_value_and_solution(self, length=10):
+        r,s = self.find_optimum_cut_bottom_up_with_first_piece(length)
+        solution = []
+        current_length = length
+        while current_length > 0:
+            solution.append(s[current_length])
+            current_length -= s[current_length]
+        return r[length],solution
 
 
 
--- dynamic/tests/test_rod_cut.py
+++ dynamic/tests/test_rod_cut.py
@@ -66,3 +66,80 @@ class TestRodCut(unittest.TestCase):
         }
         rc = RodCut(prices)
         self.assertEqual(rc.find_optimum_cut_bottom_up(length), price)
+
+    def test_find_optimum_cut_bottom_up_with_first_piece(self):
+        prices = {
+            1: 1,
+            2: 5,
+            3: 8,
+            4: 9,
+            5: 10,
+            6: 17,
+            7: 17,
+            8: 20,
+            9: 24,
+            10: 30
+
+        }
+        rc = RodCut(prices)
+        r,s = rc.find_optimum_cut_bottom_up_with_first_piece(10)
+        expected_r = {
+            0: 0,
+            1: 1,
+            2: 5,
+            3: 8,
+            4: 10,
+            5: 13,
+            6: 17,
+            7: 18,
+            8: 22,
+            9: 25,
+            10: 30
+        }
+        self.assertEqual(r, expected_r)
+        expected_s = {
+            0: 0,
+            1: 1,
+            2: 2,
+            3: 3,
+            4: 2,
+            5: 2,
+            6: 6,
+            7: 1,
+            8: 2,
+            9: 3,
+            10: 10
+        }
+        self.assertEqual(s, expected_s)
+
+    @parameterized.expand([
+        ['1', 1, 1,  [1]],
+        ['2', 2, 5,  [2]],
+        ['3', 3, 8,  [3]],
+        ['4', 4, 10, [2,2]],
+        ['5', 5, 13, [2,3]],
+        ['6', 6, 17, [6]],
+        ['7', 7, 18, [1,6]],
+        ['8', 8, 22, [2,6]],
+        ['9', 9, 25, [3,6]],
+        ['10', 10, 30, [10]]
+
+    ])
+    def test_find_rod_cut_value_and_solution(self, name, length, price, solution):
+        prices = {
+            1: 1,
+            2: 5,
+            3: 8,
+            4: 9,
+            5: 10,
+            6: 17,
+            7: 17,
+            8: 20,
+            9: 24,
+            10: 30
+
+        }
+        rc = RodCut(prices)
+        r, s = rc.find_rod_cut_value_and_solution(length)
+        self.assertEqual(r, price)
+        self.assertEqual(s, solution)
