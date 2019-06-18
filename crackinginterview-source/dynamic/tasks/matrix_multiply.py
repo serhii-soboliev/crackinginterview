344c2f4f534ab951b8a61438b1e0cc68087a1880
Serhii Sobolev <sergiy.sobolev@gmail.com>
2019-06-18 21:04:55 +0300

    [DYNAMIC] Simple matrix multiply

--- crackinginterview-source/dynamic/tasks/matrix_multiply.py
+++ crackinginterview-source/dynamic/tasks/matrix_multiply.py
@@ -6,6 +6,13 @@ class MatrixMultiply:
     @staticmethod
     def simple_matrix_multiply(a, b):
         assert np.shape(a)[1] == np.shape(b)[0], 'Number of matrix A columns should be equal to number of matrix B rows'
-        result = np.zeros((np.shape(a)[0], np.shape(b)[1]))
+        n = np.shape(a)[0]
+        m = np.shape(b)[1]
+        l = np.shape(a)[1]
+        result = np.zeros((n, m))
+        for i in range(0,n):
+            for j in range(0,m):
+                for k in range(0,l):
+                    result[i][j] += a[i][k] * b[k][j]
         return result
 
--- crackinginterview-source/tests/dynamic/test_matrix_multiply.py
+++ crackinginterview-source/tests/dynamic/test_matrix_multiply.py
@@ -20,15 +20,10 @@ class TestMatrixMultiply(unittest.TestCase):
                                      'Number of matrix A columns should be equal to number of matrix B rows'):
             MatrixMultiply.simple_matrix_multiply(a, b)
 
-    def test_simple_matrix_multiply(self):
-        a = [
-                [1,2,3],
-                [1,2,3]
-        ]
-        b = [
-                [1,2,3,4],
-                [1,2,3,4],
-                [1,2,3,4]
-        ]
-        r = MatrixMultiply.simple_matrix_multiply(a,b)
-        self.assertEqual(np.shape(r), (2,4))
+    def test_simple_matrix_multiply_random(self):
+        n = 3
+        m = 7
+        l = 9
+        a = np.random.rand(n,l)
+        b = np.random.rand(l,m)
+        np.testing.assert_array_almost_equal(np.dot(a, b), MatrixMultiply.simple_matrix_multiply(a, b))
