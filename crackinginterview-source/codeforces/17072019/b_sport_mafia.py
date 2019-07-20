aca6f94de91f530a89633be2ee4699ab4e7bff35
Serhii Sobolev <sergiy.sobolev@gmail.com>
2019-07-20 14:56:48 +0300

    [CODEFORCE] Sport mafia

--- crackinginterview-source/codeforces/17072019/b_sport_mafia.py
+++ crackinginterview-source/codeforces/17072019/b_sport_mafia.py
@@ -1,40 +1,14 @@
-def maximum_candies_after_n_movies(n):
-    return n * (n + 1) // 2
-
-
-def solve1():
-    n,k = list(map(lambda x: int(x), input().split()))
-    m = maximum_candies_after_n_movies(n)
-    current_candies = n
-    eaten_candies = 0
-    while m != k:
-        m = m - current_candies - 1
-        current_candies -= 1
-        eaten_candies += 1
-
-    print(eaten_candies)
-
-
 def solve():
     n, k = list(map(lambda x: int(x), input().split()))
-    upper_bound = n
-    lower_bound = 0
-    while upper_bound > lower_bound:
-        if upper_bound == lower_bound + 1:
-            u_c = maximum_candies_after_n_movies(upper_bound)
-            if u_c == k:
-                print(n - upper_bound)
-                break
-
-        middle = (upper_bound + lower_bound) // 2
-        m_candies = maximum_candies_after_n_movies(middle) - (n - middle)
-        if m_candies == k:
-            print(n - middle)
-            break
-        elif m_candies < k:
-            lower_bound = middle
-        else :
-            upper_bound = middle
+    upper_bound = n+1
+    lower_bound = -1
+    while upper_bound > lower_bound + 1:
+        m = (upper_bound + lower_bound) // 2
+        if (n - m) * (n - m + 1) // 2 - m > k:
+            lower_bound = m
+        else:
+            upper_bound = m
+    print(upper_bound)
 
 solve()
 
