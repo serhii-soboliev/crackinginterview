4f492ab163076fabeaf3b6d297144374f38204ef
Soboliev, Serhii <Serhii.Soboliev@McKesson.com>
2019-10-09 15:22:44 +0300

    [DYNAMIC][TOPCODER] Shortest Missing Subsequence test

---
+++ crackinginterview-source/topcoder/shortest_missing_subsequences.py
@@ -0,0 +1,10 @@
+# https://community.topcoder.com/stat?c=problem_statement&pm=15700
+
+
+class ShortestMissingSubsequences:
+
+    def build(self, g, n, a):
+        pass
+
+    def count(self, g, n, a):
+        return [1, 2]
