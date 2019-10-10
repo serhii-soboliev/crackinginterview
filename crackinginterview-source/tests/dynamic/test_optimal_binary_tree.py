5e529290e74bae72ae80adc6b8d6ace9f070f96c
Soboliev, Serhii <Serhii.Soboliev@McKesson.com>
2019-10-10 13:48:21 +0300

    [DYNAMIC] Optimal binary tree recursive solution

--- crackinginterview-source/tests/dynamic/test_optimal_binary_tree.py
+++ crackinginterview-source/tests/dynamic/test_optimal_binary_tree.py
@@ -9,11 +9,9 @@ class TestOptimalBinaryTree(unittest.TestCase):
     def setUp(self):
         self.obt = OptimalBinaryTree()
 
-
     @parameterized.expand([
         ['1', [0, 0.15, 0.1, 0.05, 0.1, 0.2], [0.05, 0.1, 0.05, 0.05, 0.05, 0.1], 2.75]
     ])
     def test_find_recursive(self, name, p, q, expected_cost):
         recursive = self.obt.find_recursive(p, q)
-        #TODO Fix obt search
-        #self.assertEqual(recursive, expected_cost)
+        self.assertEqual(recursive, expected_cost)
