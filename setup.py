bab2a26732f68833e19690ff55bfa7009aacca5f
Soboliev, Serhii <Serhii.Soboliev@McKesson.com>
2019-10-17 15:50:44 +0300

    Setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='crackinginterview',
+   version='1.0',
+   description='Preparing to interview',
+   author='Serhii Soboliev',
+   author_email='sergiy.sobolev@gmail.com',
+   packages=['crackinginterview'],  #same as name
+)
