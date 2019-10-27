e3ed0ef3663c4f326d5a9c6331d681cb9e55a3ce
Serhii Sobolev <sergiy.sobolev@gmail.com>
2019-10-27 23:54:41 +0200

    Algo folder introduced

---
+++ crackinginterview-source/algo/tests/codeforces/cr/590/test_c_pipes.py
@@ -0,0 +1,21 @@
+import unittest
+from parameterized import parameterized
+
+from algo.codeforces import is_water_flow_possible
+
+
+class TestPipes(unittest.TestCase):
+
+    @parameterized.expand([
+        ['1', 7, '2323216', '1615124', True],
+        ['2', 1, '3', '4', True],
+        ['3', 2, '13', '24', True],
+        ['4', 2, '12', '34', False],
+        ['5', 3, '536', '345', True],
+        ['6', 2, '46', '56', False],
+    ])
+    def test_pipes(self, name,  n, a, b, expected):
+        self.assertEqual(
+            is_water_flow_possible(n, a, b),
+            expected
+        )
---
+++ crackinginterview-source/algo/tests/str/test_string.py
@@ -0,0 +1,35 @@
+import unittest
+
+from parameterized import parameterized
+
+from algo.str.string import String
+
+
+class TestString(unittest.TestCase):
+
+    @parameterized.expand([
+        ['carac', "character", "carac"],
+        ['civic', "civicw", "civic"],
+        ['BABCBAB', "BBABCBCAB", "BABCBAB"],
+        ['sullus', "wswwuiollust", "sullus"],
+    ])
+    def test_find_longest_palindrome_subsequence_using_reverse(self, name, in_str, expected):
+        self.assertEqual(
+            expected,
+            String(in_str).find_longest_palindrome_subsequence_using_reverse(),
+            "Longest palindrome subsequence for {} should be {} ".format(in_str, expected)
+        )
+
+    @parameterized.expand([
+        ['carac', "character", "carac"],
+        ['civic', "civicw", "civic"],
+        ['BABCBAB', "BBABCBCAB", "BACBCAB"],
+        ['sullus', "wswwuiollust", "sullus"],
+    ])
+    def test_find_longest_palindrome_subsequence(self, name, in_str, expected):
+        self.assertEqual(
+            expected,
+            String(in_str).find_longest_palindrome_subsequence_length(),
+            "Longest palindrome subsequence for {} should be {} ".format(in_str, expected)
+        )
+
