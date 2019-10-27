aa31ee7fa764efbf1b83fa585c568b6f4319e08c
Serhii Sobolev <sergiy.sobolev@gmail.com>
2019-10-28 00:08:33 +0200

    Algo folder introduced revert

---
+++ crackinginterview-source/algo/str/string.py
@@ -0,0 +1,40 @@
+from algo.dynamic.tasks.longest_common_subsequence import LongestCommonSubSequence
+from algo.utils import EMPTY
+
+
+class String:
+    _in_str = EMPTY
+
+    def __init__(self, in_str):
+        self._in_str = in_str
+
+    def find_longest_palindrome_subsequence_using_reverse(self):
+        return LongestCommonSubSequence().lcs(str(self), self.reverse())
+
+    def find_longest_palindrome_subsequence_length(self):
+        p = self.build_longest_palindrome_subsequence_dict()
+        return p[0][len(self._in_str) - 1]
+
+    def build_longest_palindrome_subsequence_dict(self):
+        n = len(self._in_str)
+        p = [[EMPTY for _ in range(n)] for _ in range(n)]
+        for i in range(n):
+            p[i][i] = self._in_str[i]
+
+        for sublen in range(2, n+1):
+            for i in range(n-sublen+1):
+                j = i + sublen - 1
+                if self._in_str[i] == self._in_str[j]:
+                    if sublen == 2:
+                        p[i][j] = self._in_str[i] + self._in_str[j]
+                    else:
+                        p[i][j] = self._in_str[i] + p[i+1][j-1] + self._in_str[j]
+                else:
+                    p[i][j] = max(p[i+1][j], p[i][j-1], key=len)
+        return p
+
+    def __str__(self) -> str:
+        return self._in_str
+
+    def reverse(self) -> str:
+        return ''.join(reversed(self._in_str))
---
+++ crackinginterview-source/algo/tests/dynamic/test_longest_common_subsequence.py
@@ -0,0 +1,48 @@
+import unittest
+
+from parameterized import parameterized
+
+from algo.dynamic.tasks.longest_common_subsequence import LongestCommonSubSequence
+
+
+class TestLongestCommonSubsequence(unittest.TestCase):
+
+    def setUp(self):
+        self.lcs = LongestCommonSubSequence()
+
+    @parameterized.expand([
+        ['1', 'abcbdab', 'bdcaba', 'bdab']
+    ])
+    def test_find_longest_subsequence_rec(self, n, s1, s2, r):
+        self.assertEqual(self.lcs.lcs_recursive(s1, s2), r)
+
+    @parameterized.expand([
+        ['1', 'abcbdab', 'bdcaba', 'bcba'],
+        ['2', 'abACCGGTCGAGTGCGCGGAAGCCGGCCGAA', 'GTCGTTCGGAATGCCGTTGCTCTGTAAA', 'GTCGTCGGAAGCCGGCCGAA'],
+        ['3', '10010101', '010110110', '100110'],
+        ['4', '', '', ''],
+    ])
+    def test_find_longest_subsequence(self, n, s1, s2, r):
+        self.assertEqual(self.lcs.lcs(s1, s2), r)
+
+    @parameterized.expand([
+        ['1', 'abcbdab', 'bdcaba', 'bcba'],
+        ['2', 'abACCGGTCGAGTGCGCGGAAGCCGGCCGAA', 'GTCGTTCGGAATGCCGTTGCTCTGTAAA', 'GTCGTCGGAAGCCGGCCGAA'],
+        ['3', '10010101', '010110110', '100110'],
+        ['4', '', '', ''],
+        ['5', '', 'a', ''],
+        ['6', 'a', '', '']
+    ])
+    def test_find_longest_subsequence_dict(self, n, s1, s2, r):
+        self.assertEqual(self.lcs.lcs_dict(s1, s2), r)
+
+
+    @parameterized.expand([
+        ['1', 'abca', ['abc']],
+        ['1', 'ba', ['a','b']],
+        ['2', '', ['']],
+        ['3', 'cbdbf', ['bdf','cdf']],
+        ['4', 'wladbce', ['abce']],
+    ])
+    def test_increasing_lcs(self,n,s,r):
+        self.assertEqual(r, self.lcs.lis(s))
