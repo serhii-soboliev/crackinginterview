9ab05f2c5f2567521932eb75efab71d5ce3b76e8
Soboliev, Serhii <Serhii.Soboliev@McKesson.com>
2019-10-28 17:07:04 +0200

    [DYNAMIC] additional bitonic tsp test

--- crackinginterview-source/algo/tests/dynamic/test_bitonic_tsp.py
+++ crackinginterview-source/algo/tests/dynamic/test_bitonic_tsp.py
@@ -8,7 +8,8 @@ from algo.dynamic.tasks.bitonic_tsp import find_bitonic_tsp
 class TestTravelingSalesmanProblem(unittest.TestCase):
 
     @parameterized.expand([
-        ['1', [[0, 6], [1, 0], [2, 3], [5, 4], [6, 1], [7, 5], [8, 2]], [0, 1, 4, 6, 5, 3, 2]]
+        ['1', [[0, 6], [1, 0], [2, 3], [5, 4], [6, 1], [7, 5], [8, 2]], [0, 1, 4, 6, 5, 3, 2]],
+        ['2', [[0, 2], [1, 0], [2, 4], [4, 1], [5, 5]], [0, 2, 4, 3, 1]]
 
     ])
     def test_find_bitonic_tsp(self, name, p, expected_path):
