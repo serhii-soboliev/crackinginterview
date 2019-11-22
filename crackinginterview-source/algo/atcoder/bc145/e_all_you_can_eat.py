eb4f464c01b049b2eb163eb0e06fde807ab279bb
Soboliev, Serhii <Serhii.Soboliev@McKesson.com>
2019-11-22 13:32:58 +0200

    [ATCODER][BC145] E all you can eat

--- crackinginterview-source/algo/atcoder/bc145/e_all_you_can_eat.py
+++ crackinginterview-source/algo/atcoder/bc145/e_all_you_can_eat.py
@@ -1,7 +1,23 @@
-n, t = map(int, input().split())
+def all_you_can_eat(n, t, a, b):
+    dp = [[0 for _ in range(2)] for _ in range(t)]
+    for x in range(n):
+        for i in reversed(range(t)):
+            if i + a[x] < t:
+                dp[i + a[x]][0] = max(dp[i + a[x]][0], dp[i][0] + b[x])
+                dp[i + a[x]][1] = max(dp[i + a[x]][1], dp[i][1] + b[x])
+            dp[i][1] = max(dp[i][1], dp[i][0] + b[x])
+    return dp[t-1][1]
 
-a = [0 for _ in range(n)]
-b = [0 for _ in range(n)]
 
-for i in range(n):
-    a[i], b[i] = map(int, input().split())
+def solve():
+    n, t = map(int, input().split())
+    a = []
+    b = []
+    for i in range(n):
+        a[i], b[i] = map(int, input().split())
+
+    print(all_you_can_eat(n, t, a, b))
+
+
+if __name__ == "__main__":
+    solve()
---
+++ crackinginterview-source/algo/tests/atcoder/bc145/test_e_all_you_can_eat.py
@@ -0,0 +1,20 @@
+import unittest
+
+from parameterized import parameterized
+
+from algo.atcoder.bc145.e_all_you_can_eat import all_you_can_eat
+
+
+class TestAllYouCanEat(unittest.TestCase):
+
+    @parameterized.expand([
+        ['1', 2, 60, [10, 100], [10, 100], 110],
+        ['2', 3, 60, [30, 30, 30], [10, 20, 30], 50],
+        ['3', 10, 100, [15, 20, 13, 24, 18, 19, 23, 18, 27, 22], [23, 18, 17, 12, 29, 27, 21, 20, 15, 25], 145]
+    ])
+    def test_all_you_can_eat(self, name, n, t, a, b, expected):
+        self.assertEqual(
+            expected,
+            all_you_can_eat(n, t, a, b),
+            "Achievable happiness for test # {} is {}".format(name, expected)
+        )
