9770ea34afdac542c4d5c64deed2b00977d05c27
Soboliev, Serhii <Serhii.Soboliev@McKesson.com>
2019-12-18 11:43:03 +0200

    [LEETCODE][DP] 1105.Filling Bookcase Shelves refactor

--- crackinginterview-source/algo/leetcode/tasks/dp_1105_filling_bookcase_shelves.py
+++ crackinginterview-source/algo/leetcode/tasks/dp_1105_filling_bookcase_shelves.py
@@ -11,15 +11,16 @@ class MinHeightShelves:
 
         for i in range(1, n+1):
             cur_min = min_height[i - 1] + books[i-1][1]
-            start = i-1
-            while start > 0:
-                books_on_shelve = books[start-1:i]
-                lst = sum([b[0] for b in books_on_shelve])
-                if lst > shelf_width:
-                    break
-                max_height = max([b[1] for b in books_on_shelve])
+            start = i
+            books_on_shelve = books[start-1:i]
+            lst = sum([b[0] for b in books_on_shelve])
+            max_height = max([b[1] for b in books_on_shelve])
+            while start > 0 and lst <= shelf_width:
                 cur_min = min(max_height + min_height[start-1], cur_min)
                 start -= 1
+                books_on_shelve = books[start - 1:i]
+                lst = sum([b[0] for b in books_on_shelve])
+                max_height = 0 if not books_on_shelve else max([b[1] for b in books_on_shelve])
             min_height[i] = cur_min
         return min_height[n]
 
