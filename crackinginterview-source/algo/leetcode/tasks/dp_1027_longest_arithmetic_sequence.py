ddd3e21eda06f8c84698fa46a739b718f835a5bf
SerhiiSoboliev <sergiy.sobolev@gmail.com>
2019-12-27 17:48:58 +0200

    [LEETCODE][DP] 1027 - Longest Arithmetic Sequence

--- crackinginterview-source/algo/leetcode/tasks/dp_1027_longest_arithmetic_sequence.py
+++ crackinginterview-source/algo/leetcode/tasks/dp_1027_longest_arithmetic_sequence.py
@@ -2,11 +2,11 @@ from collections import defaultdict
 from typing import List
 
 
-def longest_arithmetic_sequence_length(A: List[int]) -> int:
+def longest_arithmetic_sequence_length(a: List[int]) -> int:
     dp = defaultdict(lambda: 1)
-    dp[(A[1] - A[0], 1)] = 2
-    for i in range(2, len(A)):
+    dp[(a[1] - a[0], 1)] = 2
+    for i in range(2, len(a)):
         for j in range(i):
-            interval = A[i] - A[j]
+            interval = a[i] - a[j]
             dp[(interval, i)] = 2 if (interval, j) not in dp else dp[(interval, j)] + 1
     return max(dp.values())
